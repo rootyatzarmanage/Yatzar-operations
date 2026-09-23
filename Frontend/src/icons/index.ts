98449c36104c8702b621b34d23ba4666557d88e5
vishwa-vt <vishwa.vt@yatzarcreations.com>
2026-09-23 23:17:13 +0530

    Fixes  in Teams and UI change in App Permmissions

--- Frontend/src/icons/index.ts
+++ Frontend/src/icons/index.ts
@@ -1,22 +1,28 @@
 import { ReactComponent as ChevronDownIcon } from "./chevron-down.svg?react";
 import { ReactComponent as ContactsIcon } from "./contacts.svg?react";
 import { ReactComponent as EditIcon } from "./edit.svg?react";
+import { ReactComponent as EyeIcon } from "./eye.svg?react";
 import { ReactComponent as GridIcon } from "./grid.svg?react";
 import { ReactComponent as HorizontaLDots } from "./horizontal-dots.svg?react";
 import { ReactComponent as ShieldIcon } from "./shield.svg?react";
 import { ReactComponent as TrashIcon } from "./trash.svg?react";
+import { ReactComponent as UploadIcon } from "./upload.svg?react";
 import { ReactComponent as UsFlagIcon } from "./flag-us.svg?react";
 import { ReactComponent as UsersIcon } from "./users.svg?react";
 import { ReactComponent as WorkspaceIcon } from "./workspace.svg?react";
+import { ReactComponent as FilterIcon } from "./filter.svg?react";
 
 export {
   ChevronDownIcon,
   ContactsIcon,
   EditIcon,
+  EyeIcon,
+  FilterIcon,
   GridIcon,
   HorizontaLDots,
   ShieldIcon,
   TrashIcon,
+  UploadIcon,
   UsFlagIcon,
   UsersIcon,
   WorkspaceIcon,
